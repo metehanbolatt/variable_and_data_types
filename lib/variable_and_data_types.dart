71e9a05fa36a56ec9bd77a2e10598f9ae0a12e57
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-08 11:32:57 +0300

    var - primitive types

---
+++ lib/variable_and_data_types.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  int age = 34;
+
+  /** We can declare variables without specifying the type */
+  var myAge = 24;
+  var name = "Metehan";
+  var tall = 1.75;
+  var firstLetter = "M";
+
+  String productName = "Apple Watch";
+  double productPrice = 399.9;
+
+  print("Product Name: $productName , Product Price: $productPrice");
+}
